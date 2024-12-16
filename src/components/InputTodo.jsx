777e16876b43648388a75ba36718663f734ba407
sru <simon.rutz@outlook.de>
2024-12-16 12:38:35 +0100

    Remove category changes

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,26 +1,12 @@
 /* eslint react/prop-types: 0 */
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { FaPlusCircle } from "react-icons/fa";
 
 const InputTodo = ({addTodoProps}) => {
   const [input, setInput] = useState({
     title: "",
     priority: "LOW",
-    category: "Work"
   });
-
-  const [customCategory, setCustomCategory] = useState("")
-
-  const [categories, setCategories] = useState([
-    "Work",
-    "Private",
-    "Groceries",
-    "Custom",
-  ]);
-
-  useEffect(() => {
-    input.category !== "Custom" && setCustomCategory("")
-  }, [input.category])
 
   const onChange = (e) => {
     setInput({
@@ -29,30 +15,13 @@ const InputTodo = ({addTodoProps}) => {
     });
   };
 
-  const onChangeCustomCategory = (e) => {
-    setCustomCategory(e.target.value);
-  };
-
   const handleSubmit = (e) => {
     e.preventDefault();
     if (input.title.trim()) {
-      let category
-      if (customCategory === "") {
-        category = input.category
-      } else {
-        setCategories(prev => ([
-          ...prev, 
-          customCategory
-        ]))
-
-        category = customCategory
-      }
-
-      addTodoProps({title: input.title, priority: input.priority, category});
+      addTodoProps({title: input.title, priority: input.priority});
       setInput({
         title: "",
         priority: "LOW",
-        category
       });
     } else {
       alert("Please write item");
@@ -73,19 +42,6 @@ const InputTodo = ({addTodoProps}) => {
         name="title"
         onChange={onChange}
       />
-      <select name="category" onChange={onChange}>
-        {categories.map(category => {
-          return <option value={category}>{category}</option>
-        })}
-      </select>
-      {input.category === "Custom" && <input
-        type="text"
-        className="input-text"
-        placeholder="New Category..."
-        value={customCategory}
-        name="category"
-        onChange={onChangeCustomCategory}
-      />}
       <select name="priority" onChange={onChange}>
         <option value="LOW">Low</option>
         <option value="MEDIUM">Medium</option>
