ce2a74ff7fa71e8344aabfaefa59e38217ef74b4
Leaali <ehcw2004@gmail.com>
2025-01-19 18:49:31 +0100

    Update jest test

--- src/tests/InputTodo.test.jsx
+++ src/tests/InputTodo.test.jsx
@@ -2,23 +2,30 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import InputTodo from "../components/InputTodo";
+import * as fireEvent from "react-dom/test-utils";
 
 describe('InputTodo Component', () => {
     const mockAddTodoProps = jest.fn();
     const mockSetCategories = jest.fn();
-  
+
     const categories = ['Work', 'Personal'];
-  
-    test('renders the InputTodo component correctly', () => {
-      render(
-        <InputTodo
-          addTodoProps={mockAddTodoProps}
-          categories={categories}
-          setCategories={mockSetCategories}
-        />
-      );
-  
-      expect(screen.getByPlaceholderText('Add todo...')).toBeInTheDocument();
-      expect(screen.getByPlaceholderText('Datum auswählen')).toBeInTheDocument();
+
+    beforeEach(() => {
+        render(
+            <InputTodo
+                addTodoProps={mockAddTodoProps}
+                categories={categories}
+                setCategories={mockSetCategories}
+            />
+        );
     });
+
+    test('renders the InputTodo component correctly', () => {
+        expect(screen.getByPlaceholderText('Add todo...')).toBeInTheDocument();
+        expect(screen.getByPlaceholderText('Datum auswählen')).toBeInTheDocument();
+        expect(screen.getByRole('button')).toBeInTheDocument();
+        expect(screen.getByRole('combobox', { name: /category/i })).toBeInTheDocument();
+        expect(screen.getByTestId('priority-select')).toBeInTheDocument();
+    });
+
 });
